54de8343ad1857f25f6a5e059dcc4a7db8bfb25c
Kevin Kwok <antimatter15@gmail.com>
2015-02-18 05:20:48 -0500

    first update in 5 years

--- zui.js
+++ zui.js
@@ -1,81 +1,142 @@
-var scale=1;
-var tx = innerWidth/2, ty = innerHeight/2;
-var sx = innerWidth/2, sy = innerHeight/2;
+// var box = document.createElement('div')
+// box.style.border = '5px solid red'
+// box.style.position = 'absolute'
+// box.style.top = box.style.left = '10px'
+// box.style.height = box.style.width = '10px'
+// document.body.appendChild(box)
 
-document.onmousewheel = function(e){
-  if(e.wheelDelta){
-    if(e.wheelDelta > 0) scale *= e.wheelDelta/100;
-    if(e.wheelDelta < 0) scale /= (-e.wheelDelta/100);
-  }
-  if(scale < 0.05){
-    //loadPage(historystack);
-    history.go(-1);
-  }
+var tx=0,
+  ty=0,
+  sx=innerWidth/2,
+  sy=innerHeight/2,
+  mx=innerWidth/2,
+  my=innerHeight/2;
+
+var y0 = 0, x0 = 0, 
+  x1 = innerWidth, y1 = innerHeight;
+
+
+var tar;
+
+document.onmousewheel=function(a){
+  // console.log(a.wheelDelta, scale)
+  // scale = Math.exp(Math.log(scale) + a.wheelDelta / 200)
   
+  var s = Math.exp(a.wheelDelta / 500)
+  
+  x0 = (x0 - mx) * s + mx
+  x1 = (x1 - mx) * s + mx
+
+  y0 = (y0 - my) * s + my
+  y1 = (y1 - my) * s + my
+
+  // if(Math.min(x1 - x0, y1 - y0) < 20) history.go(-1);
+  var scale = (y1-y0)/innerHeight;
+  if(scale < 0.05) history.go(-1);
+
   showTransform();
-  e.preventDefault();
-}
-
-
-window.onpopstate = function(e){
-  loadPage(e.state.url);
-}
-
+  a.preventDefault()
+};
+window.onpopstate=function(a){
+  loadPage(a.state.url)
+};
 function showTransform(){
-  document.body.style.webkitTransform='scale('+scale+') translate('+(innerWidth/2-tx)+'px,'+(innerHeight/2-ty)+'px)';
+  // box.style.left = (tx - innerWidth * scale / 2) + 'px'
+  // box.style.top = (ty - innerHeight * scale / 2) + 'px'
+
+  // box.style.width = innerWidth * scale + 'px'
+  // box.style.height = innerHeight * scale + 'px'
+
+  // box.style.left = x0 + 'px'
+  // box.style.top = y0 + 'px'
+
+  // box.style.width = (x1 - x0) + 'px'
+  // box.style.height = (y1 - y0) + 'px'
+
+  var s = (y1-y0)/innerHeight; // same as (x1-x0)/innerWidth
+
+  document.body.style.webkitTransform="scale("+s+") translate("+x0/s+"px,"+y0/s+"px)";
+  document.body.style.webkitTransformOrigin = "0 0";
+  document.body.style.overflow = 'hidden'
   
-  var cel = document.elementFromPoint(innerWidth/2,innerHeight/2);
-  var minsize = 0.30;
-  if(cel.nodeName == 'A' && cel.offsetWidth * scale > minsize  * innerWidth &&
-  cel.offsetHeight * scale > minsize * innerHeight){
-    console.log('clicky');
-    loadPage(cel.href);
-    history.pushState({url: cel.href}, cel.href, cel.href);
+  // document.body.style.webkitTransform="scale("+scale+") translate("+(innerWidth/2-tx)+"px,"+(innerHeight/2-ty)+"px)";
+  // document.body.style.webkitTransformOrigin = mx + "px " + my + "px";
+
+  // var a=document.elementFromPoint(innerWidth/2,innerHeight/2);
+  // if(a.nodeName=="A"&&a.offsetWidth*scale>0.3*innerWidth&&a.offsetHeight*scale>0.3*innerHeight){
+  //  console.log("clicky");
+  //  loadPage(a.href);
+  //  history.pushState({url:a.href},a.href,a.href)
+  // }
+
+  if(tar && tar.nodeName == 'A' && 1/s < 0.05){
+    console.log("clicky");
+    loadPage(tar.href);
+    history.pushState({url:tar.href},tar.href,tar.href)
   }
+
+} 
+function loadPage(a){
+  var b=new XMLHttpRequest;
+  b.open("get",a,true);
+  document.body.innerHTML=''
+  b.onload=function(){
+    document.body.innerHTML=b.responseText;
+    resetParameters()
+    showTransform()
+  };
+  b.send(null);
+  resetParameters()
+  showTransform()
 }
+var dragging=false;
+document.onmousemove=function(a){
+  // a.pageX
+  // a.pageY
+  // console.log(a.pageX, a.pageY)
+  mx = a.pageX
+  my = a.pageY
+  // tx = a.pageX
+  // ty = a.pageY
+  // showTransform()
+  tar = a.target;
 
-
-function loadPage(href){
-  var xhr = new XMLHttpRequest();
-  xhr.open('get', href, true);
-  xhr.onload = function(){
-    document.body.innerHTML = xhr.responseText;
-    showTransform();
-  }
-  xhr.send(null);
-  scale = 1;
-  tx = innerWidth/2;
-  ty = innerHeight/2;
-  sx = innerWidth/2; 
-  sy = innerHeight/2;
-  showTransform();
-}
-
-var dragging = false;
-document.onmousemove=function(e){
   if(dragging){
-    tx += (sx - e.pageX) / scale;
-    ty += (sy - e.pageY) / scale;
-    sx = e.pageX;
-    sy = e.pageY;
+    var s = (y1-y0)/innerHeight;
+    var dx = (sx-a.pageX),
+      dy = (sy-a.pageY);
+
+    x0-=dx;
+    x1-= dx;
+    y0-= dy;
+    y1-=dy;
+    sx=a.pageX;
+    sy=a.pageY;
     showTransform();
-    e.preventDefault();
-    e.stopPropagation();
+    a.preventDefault();
+    a.stopPropagation()
   }
-}
+}; 
 
-document.onmousedown = function(e){
-  dragging = true;
-  sx = e.pageX;
-  sy = e.pageY;
-  e.preventDefault();
+function resetParameters(){
+  tx=0;
+  ty=0;
+  sx=innerWidth/2;
+  sy=innerHeight/2;
+  y0 = 0, x0 = 0, 
+  x1 = innerWidth;
+  y1 = innerHeight;
 }
-
-document.onclick = function(e){
-  e.preventDefault();
-}
-
-document.onmouseup = function(e){
-  dragging = false;
-  e.preventDefault();
-}
+document.onmousedown=function(a){
+  dragging=true;
+  sx=a.pageX;
+  sy=a.pageY;
+  a.preventDefault()
+};
+document.onclick=function(a){
+  a.preventDefault()
+};
+document.onmouseup=function(a){
+  dragging=false;
+  a.preventDefault()
+};
